f51caf1e9bd84a4ae6e011f6b17e10f593b99496
KaiHao Chen <chenkenny414@gmail.com>
2025-07-04 09:52:51 -0400

    new ui layout

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './navbar.css';
-import cart from '../../assets/cart.png'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => { 
   return (
     <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo" />
       <ul className='navbar-menu'>
         <li>Home</li>
         <li>Shop</li>
-        <li>Contacts</li>
+        <li>Contact</li>
+        <li>Purpose</li>
       </ul>
-      <div className="navbar-right">
-        <img src={cart} alt="" />
+      <div className='navbar-right'>
+        <img src={assets.search_icon} alt="" />
+        <div className='navbar-search-icon'>
+            <img src={assets.basket_icon} alt="" />
+            <div className='dot'></div>
+        </div>
+        <button>Sign In</button>
       </div>
     </div>
   );
