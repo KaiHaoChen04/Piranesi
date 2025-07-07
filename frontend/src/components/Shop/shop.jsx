fe5b3950b4be005016042b3b8b0e0aed2a2c9ce5
KaiHao Chen <chenkenny414@gmail.com>
2025-07-07 14:43:37 -0400

    added shop collections

--- frontend/src/components/Shop/shop.jsx
+++ frontend/src/components/Shop/shop.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './shop.css'
+import { menu_list } from '../../assets/assets'
 
 const shop = () => {
   return (
-    <div>
-      
+    <div className='shop-menu' id = 'shop-menu'>
+      <h1>Explore Our Latest Collections</h1>
+      <div className='Collection-Lists'>
+        {menu_list.map((item, index)=>{
+            return (
+                <div key = {index}className='shop-menu-list-items'>
+                    <img src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
     </div>
   )
 }
 
 export default shop
+
--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import Header from '../../components/Header/header'
+import Shop from '../../components/Shop/shop'
 
 const home = () => {
   return (
     <div>
       <Header/>
+      <Shop/>
     </div>
   )
 }
