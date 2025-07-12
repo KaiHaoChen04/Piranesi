d0162a2956d6ee0309aa965ebeac3b776762eccf
KaiHao Chen <chenkenny414@gmail.com>
2025-07-12 15:20:25 -0400

    added addToCart method so every prev state will update to a new state

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,10 +1,23 @@
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
+import cart from "../pages/Cart/cart";
 
 export const StoreContext = createContext(null);
 
 
 const StoreContextProvider = (props) => {
+
+    const [cartItem, setCartItem] = useState({});
+
+    const addToCart = (itemID) => {
+        if(!cartItem[itemID]){
+            setCartItem((prev)=>({...prev,[itemID]:1}))
+        }
+        else{
+            setCartItem((prev)=>({...prev,[itemID]:prev[itemID]+1}))
+        }
+    }
+
     const contextValue = {
         food_list
     }
