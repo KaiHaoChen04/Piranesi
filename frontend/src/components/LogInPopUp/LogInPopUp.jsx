4fa9e7ffc6902427e740e75771f281136cecef06
KaiHao Chen <chenkenny414@gmail.com>
2025-07-17 17:13:11 -0400

    mounted and unmounted login currentState

--- frontend/src/components/LogInPopUp/LogInPopUp.jsx
+++ frontend/src/components/LogInPopUp/LogInPopUp.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './LogInPopUp.css'
+import {useState} from 'react';
+import { assets } from '../../assets/assets';
 
-const LogInPopUp = () => {
+const LogInPopUp = ({setShowLogin}) => {
+    const [currentState, setCurrentState] = useState("Sign up")
   return (
     <div className='LogInPopUp'>
-      Login
+        <form className='login-container'>
+            <div className="login-title">
+                <h2>{currentState}</h2>
+                <img onClick={()=>setShowLogin(false)}src={assets.cross_icon} alt="" />
+            </div>
+        </form>
     </div>
   )
 }
