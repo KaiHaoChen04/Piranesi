e9a6e456cae41aa5c1dfcbf19a55fe682f8d8f04
KaiHao Chen <chenkenny414@gmail.com>
2025-07-18 14:53:41 -0400

    input fields added

--- frontend/src/components/LogInPopUp/LogInPopUp.jsx
+++ frontend/src/components/LogInPopUp/LogInPopUp.jsx
@@ -12,6 +12,12 @@ const LogInPopUp = ({setShowLogin}) => {
                 <h2>{currentState}</h2>
                 <img onClick={()=>setShowLogin(false)}src={assets.cross_icon} alt="" />
             </div>
+            <div className='login-input'>
+                <input type="text" placeholder='Your Name' required/>
+                <input type="email" placeholder='Your email' required/>
+                <input type="password" placeholder='Your Password' required />
+            </div>
+            <button>{currentState==="Sign Up"?"Create Account":"Login"}</button>
         </form>
     </div>
   )
