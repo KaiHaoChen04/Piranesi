d8c632fc22f8200321d05c702e9de0c1acb4021a
KaiHao Chen <chenkenny414@gmail.com>
2025-07-28 10:51:07 -0400

    injected dependencies for backend

--- frontend/src/pages/PlaceOrder/placeOrder.jsx
+++ frontend/src/pages/PlaceOrder/placeOrder.jsx
@@ -36,13 +36,13 @@ const placeOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>$ {2}</p>
+              <p>$ {getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <br />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>$ {getTotalCartAmount() + 2}</b>
+              <b>$ {getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
           <button>
