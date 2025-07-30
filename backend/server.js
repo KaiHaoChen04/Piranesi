6924d47c14d472760d6a3c890436921058263c02
KaiHao Chen <chenkenny414@gmail.com>
2025-07-30 11:03:29 -0400

    atlas db setup and schema completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { connectDB } from './config/db.js'
 
 
 // app config
@@ -7,7 +8,18 @@ const app = express()
 const port = 4000
 
 //middleware
-app.use(express.json) //Used to handle requests from frontend
+app.use(express.json()) //Used to handle requests from frontend
 app.use(cors()) // Access the backend from any frontend
 
+// db connection
+connectDB()
 
+app.get("/", (req, res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=>{
+    console.log('server started on http://localhost:{port}');
+})
+
+// mongodb+srv://kaihao:KennyChen123@cluster0.nyojt9k.mongodb.net/?
