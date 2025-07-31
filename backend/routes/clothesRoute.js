7aa3a3b787e61a4395ea51c53e72cc76b29a8c3d
KaiHao Chen <chenkenny414@gmail.com>
2025-07-31 11:36:43 -0400

    post method and unique image filename function created

--- backend/routes/clothesRoute.js
+++ backend/routes/clothesRoute.js
@@ -4,5 +4,12 @@ import multer from "multer"
 
 const clothesRouter = express.Router(); // GET, POST, etc... methods
 
+//Image storage engine
+const storage = multer.diskStorage({
+    destination:"uploads",
+    filename: (req,file,cb)=>{return cb(null,`${Date.now()}${file.originalname}`)} //This arrow function makes our filename unique because every TIME is different
+})
+
+clothesRouter.post('/add', addClothes) //Send data to the server and once the data gets processed we will get a response
 
 export default clothesRouter;
