9e74f701bc8ca5b7e0784f8e48d87beaeceae55c
KaiHao Chen <chenkenny414@gmail.com>
2025-08-05 20:04:24 -0400

    updated assets.js

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,11 @@
+import add_icon from './add_icon.png'
+import admin_image from './admin.png'
+import parcel_icon from './parcel_icon.png'
+import upload_icon from './upload_icon.png'
+
+export const assets = {
+    add_icon,
+    admin_image,
+    parcel_icon,
+    upload_icon
+}
