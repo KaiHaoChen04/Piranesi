ea2b9172f0b36e46665462e2f9e9479e56a1f830
KaiHao Chen <chenkenny414@gmail.com>
2025-08-08 14:09:08 -0400

    sidebar image and imports done

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -2,10 +2,12 @@ import add_icon from './add_icon.png'
 import admin_image from './admin.png'
 import parcel_icon from './parcel_icon.png'
 import upload_icon from './upload_icon.png'
+import order_icon from './order_icon.png'
 
 export const assets = {
     add_icon,
     admin_image,
     parcel_icon,
-    upload_icon
+    upload_icon,
+    order_icon
 }
