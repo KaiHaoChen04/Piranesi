643126984f8e7f16f1d128f758252e5e5374beea
KaiHao Chen <chenkenny414@gmail.com>
2025-08-11 12:25:38 -0400

    Finished admin panel 'add' layout jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import './Add.css'
+import { assets } from '../../assets/assets'
 
 const Add = () => {
   return (
-    <div>
-      
+    <div className='add'>
+      <form className='flex-col'>
+        <div className="add-image-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_icon} alt=""/>
+          </label>
+          <input type="file" id="image" hidden required/>
+        </div>
+        <div className='add-product-name flex-col'>
+          <p>Product Name</p>
+          <input type="text" name="name" placeholder='Type here'/>
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product Description</p>
+          <textarea name="description" rows="6" placeholder='Content' required></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product Category</p>
+            <select name="category">
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+            </select>
+          </div>
+          <div className="add-price flex col">
+            <p>Product price</p>
+            <input type="number" name='price' placeholder='$'/>
+          </div>
+        </div>
+        <button type='submit' className='add-bottom'>ADD</button>
+      </form>
     </div>
   )
 }
