2b1b51702bcd5e0ec479c9f59b556dd307b659bf
KaiHao Chen <chenkenny414@gmail.com>
2025-08-18 16:38:21 -0400

    fetched clothes list via axios through clothes api

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './List.css'
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 const List = () => {
+
+  const [list, setList] = useState([]); {/* Array for storing our food lists */}
+
+  const url = "http://localhost:4000" 
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/clothes/list`); {/* Fetch all the clothes item from our backend */}
+    if(response.data.success){
+      setList(response.data.data);
+    }
+    else{
+      toast.error("error");
+    }
+  }
+
   return (
-    <div>
+    <div className='list'>
       
     </div>
   )
