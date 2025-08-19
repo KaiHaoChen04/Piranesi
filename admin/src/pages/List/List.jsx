8ed8144cb5901a1020ef5c35092fc2f376f0258c
KaiHao Chen <chenkenny414@gmail.com>
2025-08-19 10:34:16 -0400

    mapped object array into list API

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './List.css'
 import axios from 'axios'
 import {toast} from 'react-toastify'
@@ -18,18 +18,32 @@ const List = () => {
       toast.error("error");
     }
   }
+  useEffect(() => {
+    fetchList(); {/* We pass in an empty array so the useEffect runs only once and won't ever run again even if it re-renders */}
+  }, []);
 
   return (
     <div className='list add flex-col'>
       <p>All Clothes</p>
       <div className='list-table'>
-        <div className="list-table-title">
+        <div className="list-table-format title">
           <b>Image</b>
           <b>Name</b>
           <b>Category</b>
           <b>Price</b>
           <b>Action</b>
         </div>
+        {list.map((item, index) => {
+          return (
+            <div key = {index} className='list-table-format'>
+              <img src={`${url}/images/`+item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p>X</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
