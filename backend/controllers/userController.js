b82fc7087b5d66e79fddbda52f231c7ddad78dcb
KaiHao Chen <chenkenny414@gmail.com>
2025-08-24 19:51:21 -0400

    email and password validator

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,25 @@ const loginUser = async (req,res) => {
 
 //sign up
 const registerUser = async(req,res) => {
-
+    const {name,password,email} = req.body;
+    try {
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({success:false, message:"User already exists"})
+        }
+        if(!validator.isEmail(email)){
+            return res.json({success:false, message:"Not an email"})
+        }
+        if(password.length<8){
+            return res.json({success:false, message:"Password too short"})
+        }
+        
+        else{
+            return res.json({success:true,message:"Register successful"})
+        }
+    } catch (error) {
+        
+    }
 }
 
 export {loginUser, registerUser};
