f3d8ab6700eb3b2cc2ca34f12323848c91367e52
KaiHao Chen <chenkenny414@gmail.com>
2025-09-09 19:58:33 -0400

    cartController initialized

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,17 @@
+import userModel from "../models/userModel.js";
+
+
+//Add to cart
+const addToCart = async (req, res) => {
+
+}
+
+//Remove from cart
+const removeFromCart = async(req, res) => {
+
+}
+
+//Get cart
+const getCart = async(req, res) => {
+    
+}
