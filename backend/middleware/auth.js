d0c10e979baa204b85e1f065de3d3aaf679dd416
KaiHao Chen <chenkenny414@gmail.com>
2025-09-15 18:30:27 -0400

    authenticating using jwt and storing userid data

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,18 @@
 import jwt from 'jsonwebtoken'
 
 const authMiddleware = async (req,res,next) => {
-    
+    const {token} = req.headers;
+    if(!token){
+        return res.json({success:false,message:"Not authorized"});
+    }
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = tokenDecode.id;
+        next(); // pass control to the next middleware/route handler 
+    } catch (error) {
+        console.log(error);
+        return res.json({success:false,message:"Error"});
+    }
 }
 
 export default authMiddleware;
