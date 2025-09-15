b83d57641d3e6c3c7c81d32815e4d8b03085706c
KaiHao Chen <chenkenny414@gmail.com>
2025-09-15 18:29:33 -0400

    find userId and add to its cart data entries

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,7 +3,19 @@ import userModel from "../models/userModel.js";
 
 //Add to cart
 const addToCart = async (req, res) => {
+    try {
+        let userData = await userModel.findOne({_id:req.body.userId}); //We will get this from the middleware
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId]){ //If no entries
 
+            cartData[req.body.itemId] = 1 //Create new entry
+        }
+        else{
+            cartData[req.body.itemId] += 1; //Add onto existing entry
+        }
+    } catch (error) {
+        
+    }
 }
 
 //Remove from cart
