eb3ceda35027cecd8dbef271df8d0a6668e08e12
KaiHao Chen <chenkenny414@gmail.com>
2025-09-20 18:08:28 -0400

    Update removefromcart function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -4,11 +4,11 @@ import userModel from "../models/userModel.js";
 //Add to cart
 const addToCart = async (req, res) => {
     try {
-        let userData = await userModel.findOne({_id:req.body.userId}); //We will get this from the middleware
+        let userData = await userModel.findById(req.body.userId); //We will get this from the middleware
         let cartData = await userData.cartData;
         if(!cartData[req.body.itemId]){ //If no entries
 
-            cartData[req.body.itemId] = 1 //Create new entry
+            cartData[req.body.itemId] = 1; //Create new entry
         }
         else{
             cartData[req.body.itemId] += 1; //Add onto existing entry
@@ -22,7 +22,17 @@ const addToCart = async (req, res) => {
 
 //Remove from cart
 const removeFromCart = async(req, res) => {
-
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if(cartData[req.body.itemId] > 0){
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        res.json({success:true, message:"Removed from cart"});
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 //Get cart
